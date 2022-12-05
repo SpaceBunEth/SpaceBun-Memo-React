3df178920edc5834516efef622331b8a0dde1cba
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-12-05 16:03:07 +0000

    added routers to index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
-
+import './index.css';
+import App from './App';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './components/user/Login';
+import Register from './components/user/Register';
+import Profile from './components/user/Profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-  <>hello</>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
