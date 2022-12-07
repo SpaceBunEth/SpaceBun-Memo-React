02908eb48c40b8af8ce7a79834ff3f2f309eca7f
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-12-07 21:36:02 +0000

    working on editting profile functionality

--- app/src/components/user/EditProfile.js
+++ app/src/components/user/EditProfile.js
@@ -7,6 +7,18 @@ const EditProfile = () => {
 return(
     <>
     <h1>Edit Profile</h1>
+    <label htmlFor="body">DisplayName</label>
+    <br/>
+    <input type="text" id="body" name="body"></input>
+    <br/>
+    <label htmlFor="body">Bio</label>
+    <br/>
+    <input type="text" id="body" name="body"></input>
+    <br/>
+    <label htmlFor="body">PFP</label>
+    <br/>
+    <input type="text" id="body" name="body"></input>
+
     <Link to="/profile">Return/Submit to profile</Link>
     </>
 )}
